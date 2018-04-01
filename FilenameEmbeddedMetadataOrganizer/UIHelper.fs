901df382b606c35e155026b3b61590f8598396c9
TeaDrivenDev <TeaDrivenDev@gmail.com>
2018-04-01 05:02:25 +0200

    Add file size column

--- FilenameEmbeddedMetadataOrganizer/UIHelper.fs
+++ FilenameEmbeddedMetadataOrganizer/UIHelper.fs
@@ -10,8 +10,6 @@ open FilenameEmbeddedMetadataOrganizer.ViewModels
 type FeaturesTreeViewItemTemplateSelector() =
     inherit DataTemplateSelector()
 
-    static member Instance = FeaturesTreeViewItemTemplateSelector()
-
     override __.SelectTemplate(item : obj, container : DependencyObject) =
         match item with
         | :? FeatureInstanceViewModel -> __.FeatureInstanceTemplate
@@ -22,7 +20,9 @@ type FeaturesTreeViewItemTemplateSelector() =
 
     member val FeatureInstanceTemplate = Unchecked.defaultof<DataTemplate> with get, set
 
-type BooleanToVisibilityConverter() =
+type InvertableBooleanToVisibilityConverter() =
+    static member Instance = InvertableBooleanToVisibilityConverter() :> IValueConverter
+
     interface IValueConverter with
         member this.Convert(value: obj, targetType: System.Type, parameter: obj, culture: System.Globalization.CultureInfo): obj =
             if value = DependencyProperty.UnsetValue
@@ -53,4 +53,17 @@ type RelativePathConverter() =
             :> obj
 
         member this.ConvertBack(value: obj, targetTypes: System.Type [], parameter: obj, culture: System.Globalization.CultureInfo): obj [] =
+            raise (System.NotImplementedException())
+
+type BytesToMegabytesConverter() =
+    static member Instance = BytesToMegabytesConverter() :> IValueConverter
+
+    interface IValueConverter with
+        member this.Convert(value: obj, targetType: Type, parameter: obj, culture: Globalization.CultureInfo): obj =
+            if value = DependencyProperty.UnsetValue
+            then 0.
+            else (System.Convert.ToDouble value) / 1_000_000.
+            :> obj
+
+        member this.ConvertBack(value: obj, targetType: Type, parameter: obj, culture: Globalization.CultureInfo): obj =
             raise (System.NotImplementedException())

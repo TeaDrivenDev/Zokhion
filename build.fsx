ee3e32f4d003d460f1b9cc6c459310e9594023c2
TeaDrivenDev <TeaDrivenDev@gmail.com>
2018-10-21 07:26:08 +0200

    Allow specifying build configuration for build script

--- build.fsx
+++ build.fsx
@@ -27,17 +27,19 @@ let gitRaw = Environment.environVarOrDefault "gitRaw" ("https://raw.github.com/"
 
 let outputDirectory = "bin"
 
+let configuration = Environment.environVarOrDefault "Configuration" "Release"
+
 Target.create "Restore" (fun _ -> Paket.restore id)
 
 Target.create "Clean" (fun _ -> Shell.cleanDirs [ outputDirectory ])
 
 Target.create "Build" (fun _ ->
     !! solutionFile
-    |> MSBuild.runWithDefaults "Rebuild"
+    |> MSBuild.run id "" "Rebuild" [ "Configuration", configuration ]
     |> ignore)
 
 Target.create "RunTests" (fun _ ->
-    !! "**/bin/Release/**/*.Tests.dll"
+    !! (sprintf "**/bin/%s/**/*.Tests.dll" configuration)
     |> XUnit2.run id)
 
 Target.create "All" ignore

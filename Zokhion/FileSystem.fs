8afd1d57a96ffe984d4696931e7454eb1f5a19df
TeaDrivenDev <TeaDrivenDev@gmail.com>
2020-11-15 20:55:19 +0100

    Remove references to System.IO from UIHelper.fs

---
+++ Zokhion/FileSystem.fs
@@ -0,0 +1,10 @@
+﻿namespace TeaDriven.Zokhion.FileSystem
+
+open System.IO
+
+module Utilities =
+
+    let inline getDirectoryName path = Path.GetDirectoryName path
+
+    let inline getFileNameWithoutExtension path =
+        Path.GetFileNameWithoutExtension path

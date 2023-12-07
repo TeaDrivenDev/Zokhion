297aa86ff3901545fb3a2381c22da33034ed0c93
TeaDrivenDev <TeaDrivenDev@gmail.com>
2023-12-07 01:56:08 +0100

    Split large tests file

---
+++ Zokhion.Tests/SettingsTests.fs
@@ -0,0 +1,119 @@
+﻿namespace TeaDriven.Zokhion.Tests
+
+module SettingsTests =
+    open Xunit
+
+    open TeaDriven.Zokhion.Settings
+
+    [<Fact>]
+    let ``A feature hierarchy is serialized in the correct format`` () =
+        // Arrange
+        let values =
+            [
+                {
+                    Name = "Scene"
+                    Code = "S"
+                    Include = None
+                    Instances =
+                        [
+                            { Name = "Sunset"; Code = "Ss" }
+                            { Name = "Pre-dawn"; Code = "Pd" }
+                        ]
+                }
+
+                {
+                    Name = "Type of shot"
+                    Code = "T"
+                    Include = Some "XAb"
+                    Instances =
+                        [
+                            { Name = "Aerial, distant"; Code = "Ad" }
+                            { Name = "Aerial, overhead"; Code = "Ao" }
+                            { Name = "Ground level, distant"; Code = "Gd" }
+                        ]
+                }
+            ]
+
+        let expectedResult =
+            [
+                "S|Scene"
+                "	SSs|Sunset"
+                "	SPd|Pre-dawn"
+                "T|Type of shot > XAb"
+                "	TAd|Aerial, distant"
+                "	TAo|Aerial, overhead"
+                "	TGd|Ground level, distant"
+            ]
+
+        // Act
+        let result: string list = serializeFeatures values
+
+        // Assert
+        Assert.StrictEqual (expectedResult, result)
+
+    [<Fact>]
+    let ``A feature list without instances is correctly deserialized`` () =
+        // Arrange
+        let values =
+            [
+                "S|Scene"
+                "T|Type of shot > XAb"
+            ]
+
+        let expectedResult =
+            [
+                { Name = "Scene"; Code = "S"; Include = None; Instances = [] }
+                { Name = "Type of shot"; Code = "T"; Include = Some "XAb"; Instances = [] }
+            ]
+
+        // Act
+        let result = deserializeFeatures values
+
+        // Assert
+        Assert.StrictEqual (expectedResult, result)
+
+    [<Fact>]
+    let ``A feature hierarchy is correctly deserialized`` () =
+        // Arrange
+        let values =
+            [
+                "S|Scene"
+                "	SSs|Sunset"
+                "	SPd|Pre-dawn"
+                "T|Type of shot > XAb"
+                "	TAd|Aerial, distant"
+                "	TAo|Aerial, overhead"
+                "	TGd|Ground level, distant"
+            ]
+
+        let expectedResult =
+            [
+                {
+                    Name = "Scene"
+                    Code = "S"
+                    Include = None
+                    Instances =
+                        [
+                            { Name = "Sunset"; Code = "Ss" }
+                            { Name = "Pre-dawn"; Code = "Pd" }
+                        ]
+                }
+
+                {
+                    Name = "Type of shot"
+                    Code = "T"
+                    Include = Some "XAb"
+                    Instances =
+                        [
+                            { Name = "Aerial, distant"; Code = "Ad" }
+                            { Name = "Aerial, overhead"; Code = "Ao" }
+                            { Name = "Ground level, distant"; Code = "Gd" }
+                        ]
+                }
+            ]
+
+        // Act
+        let result = deserializeFeatures values
+
+        // Assert
+        Assert.StrictEqual (expectedResult, result)
---
+++ Zokhion.Tests/SplitFileNameTests.fs
@@ -0,0 +1,48 @@
+﻿namespace TeaDriven.Zokhion.Tests
+
+module SplitFileNameTests =
+    open Xunit
+
+    open TeaDriven.Zokhion
+
+    [<Theory>]
+    [<InlineData("Aerial view over Uluru at night", "Aerial view over Uluru at night", "", "")>]
+    [<InlineData("Aerial view (from above) over Uluru at night", "Aerial view (from above) over Uluru at night", "", "")>]
+    [<InlineData("Aerial view over Uluru at night (Uluru)", "Aerial view over Uluru at night", "(Uluru)", "")>]
+    [<InlineData("Aerial view over Uluru at night (.Andes.Pacific Ocean.)", "Aerial view over Uluru at night", "(.Andes.Pacific Ocean.)", "")>]
+    [<InlineData("Aerial view (from above) over Uluru at night (.Andes.Pacific Ocean.)", "Aerial view (from above) over Uluru at night", "(.Andes.Pacific Ocean.)", "")>]
+    [<InlineData("Aerial view over Uluru at night (.Andes.Pacific Ocean.) [.Ax.Bd.]", "Aerial view over Uluru at night", "(.Andes.Pacific Ocean.)", "[.Ax.Bd.]")>]
+    [<InlineData("Aerial view over Uluru at night [.Ax.Bd.]", "Aerial view over Uluru at night", "", "[.Ax.Bd.]")>]
+    [<InlineData("Aerial view over Uluru at night [NoFeature]", "Aerial view over Uluru at night [NoFeature]", "", "")>]
+    let ``Filename parts are correctly detected`` (fileName, expectedMain, expectedNames, expectedFeatures) =
+        // Arrange
+        let fileName = fileName
+
+        let expectedResult = (expectedMain, expectedNames, expectedFeatures)
+
+        // Act
+        let result = splitFileName true fileName
+
+        // Assert
+        Assert.Equal (expectedResult, result)
+
+    [<Theory>]
+    [<InlineData("Aerial view over Uluru at night", "Aerial view over Uluru at night", "", "")>]
+    [<InlineData("Aerial view (from above) over Uluru at night", "Aerial view (from above) over Uluru at night", "", "")>]
+    [<InlineData("Aerial view over Uluru at night (Uluru)", "Aerial view over Uluru at night (Uluru)", "", "")>]
+    [<InlineData("Aerial view over Uluru at night (.Andes.Pacific Ocean.)", "Aerial view over Uluru at night (.Andes.Pacific Ocean.)", "", "")>]
+    [<InlineData("Aerial view (from above) over Uluru at night (.Andes.Pacific Ocean.)", "Aerial view (from above) over Uluru at night (.Andes.Pacific Ocean.)", "", "")>]
+    [<InlineData("Aerial view over Uluru at night (.Andes.Pacific Ocean.) [.Ax.Bd.]", "Aerial view over Uluru at night (.Andes.Pacific Ocean.)", "", "[.Ax.Bd.]")>]
+    [<InlineData("Aerial view over Uluru at night [.Ax.Bd.]", "Aerial view over Uluru at night", "", "[.Ax.Bd.]")>]
+    [<InlineData("Aerial view over Uluru at night [NoFeature]", "Aerial view over Uluru at night [NoFeature]", "", "")>]
+    let ``Filename parts are correctly detected omitting separate name part`` (fileName, expectedMain, expectedNames, expectedFeatures) =
+        // Arrange
+        let fileName = fileName
+
+        let expectedResult = (expectedMain, expectedNames, expectedFeatures)
+
+        // Act
+        let result = splitFileName false fileName
+
+        // Assert
+        Assert.Equal (expectedResult, result)
